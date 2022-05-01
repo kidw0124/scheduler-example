530e45300ed6dfa7f8ac4c9719b0ccb778a7d431
yurimn <leeyou6757@gmail.com>
2022-05-01 23:56:54 +0900

    라우팅

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./Login";
+import Error from "./Error";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import { useState } from "react";
 
 function App() {
+	const [userDt, setUserDt] = useState({
+		logined: false,
+		name: undefined,
+	});
 	return (
 		<div className="App">
-			<Login />
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/"
+						element={<Home userDt={userDt} setUserDt={setUserDt} />}
+					/>
+					<Route
+						path="/login"
+						element={
+							<Login userDt={userDt} setUserDt={setUserDt} />
+						}
+					/>
+					<Route
+						path="/home"
+						element={<Home userDt={userDt} setUserDt={setUserDt} />}
+					/>
+					<Route
+						path="*"
+						element={
+							<Error errno={404} errdsc={"There is no page."} />
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
